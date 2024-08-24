82786d1fa0f7cd1c041d2341eb9cef01c8195253
Vasteron-git <Mp.selvester@gmail.com>
2024-08-24 19:34:52 +0300

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,18 @@
+# 1st program комментарий
+print('1th program:')
+print( 9 ** 0.5 )
+print( 9 ** 0.5 * 5 )
+
+# 2nd program
+print('2th program:')
+print(9.99 > 9.99)
+
+# 3rd program Школьная загадка
+print('3th program:')
+print( 2 * 2 + 2 )
+print( 2 * (2 + 2) )
+print( (2 * 2 + 2)  ==  (2 * (2 + 2)))
+
+# 4th program
+print('4th program:')
+print(int(123.456 * 10 % 10))
